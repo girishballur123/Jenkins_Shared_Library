b764af88be2b691c25a93ae5d1057b6ce2100c3a
Darshan8050 <Darshan.J@sapiens.com>
2024-12-14 07:57:39 +0530

    changed script

--- constants.groovy
+++ constants.groovy
@@ -1,3 +1,13 @@
+// vars/constants.groovy
 import groovy.transform.Field
+
 @Field String gitUrl = "https://github.com/devops5014/sample-webapp.git"
 @Field String token = "github-token"
+
+def getGitUrl() {
+    return gitUrl
+}
+
+def getToken() {
+    return token
+}
