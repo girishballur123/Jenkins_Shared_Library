d7db425c915db087f92cea3ae734fc60277c5c27
Darshan8050 <Darshan.J@sapiens.com>
2024-12-14 08:03:38 +0530

    added new directory called vars

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -4,10 +4,3 @@ import groovy.transform.Field
 @Field String gitUrl = "https://github.com/devops5014/sample-webapp.git"
 @Field String token = "github-token"
 
-def getGitUrl() {
-    return gitUrl
-}
-
-def getToken() {
-    return token
-}
