db0387a5ced577b4818076dc49063e383b4b8c0f
girishballur123 <135659826+girishballur123@users.noreply.github.com>
2025-02-03 23:18:45 +0530

    Update constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -1,14 +1,14 @@
 // vars/constants.groovy
 import groovy.transform.Field
 
-@Field String AWS_ACCOUNT_ID = '816069160812'
-@Field String AWS_DEFAULT_REGION = 'ap-south-1'
+@Field String AWS_ACCOUNT_ID = '207567760307'
+@Field String AWS_DEFAULT_REGION = 'us-east-1'
 @Field String IMAGE_REPO_NAME = 'sample-webapp'
-@Field String GIT_REPO_URL = 'https://github.com/Sumukha97/sample-webapp.git'
+@Field String GIT_REPO_URL = 'https://github.com/girishballur123/Maven-project.git'
 
 // Jenkins Credential IDs
-@Field String GIT_CREDENTIALS_ID = 'jenkinsfile1'
-@Field String ECR_CREDENTIALS_ID = 'aws-credentials'
+@Field String GIT_CREDENTIALS_ID = 'Jenkinslatest'
+@Field String ECR_CREDENTIALS_ID = 'AWS_Credentials'
 
 def call() {
     return this
